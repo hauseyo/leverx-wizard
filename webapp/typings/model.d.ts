de19660a64dd6cb5034736f3929c7f18b4b9eda5
hauseyo <rosadeyventi@gmail.com>
2021-02-11 12:38:18 +0300

    fix: remove unuser class

--- webapp/typings/model.d.ts
+++ webapp/typings/model.d.ts
@@ -1,8 +1,4 @@
 declare namespace Model {
-  export class CommonControl extends sap.ui.base.EventProvider {
-    public data(dataName: string): string;
-  }
-
   declare namespace view {
     interface State {
       currentStep: number;
